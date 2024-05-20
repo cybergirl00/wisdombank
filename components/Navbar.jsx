b5b9ab71c2dfbe3bad77507252d6a8e97a7a7535
cybergirl00 <92691928+cybergirl00@users.noreply.github.com>
2024-05-20 09:02:16 +0100

    first commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -18,17 +18,16 @@ const Navbar = () => {
   return (
     <div className='shadow-sm flex justify-between p-3 sticky z-10 top-0 bg-white'>
         <div className="text-2xl font-bold p-1 ">Wisdom <span className='text-primary'>Bank</span></div>
-        <div className=" w-full max-w-sm items-center space-x-2 hidden lg:flex">
+        {/* <div className=" w-full max-w-sm items-center space-x-2 hidden lg:flex">
       <Input type="search" placeholder="Search..." />
       <Button type="submit">
-         {/* <Search size={15}/> */}
        Search</Button>
-    </div>
+    </div> */}
         <div className="">
             <ul className='flex items-center gap-3'>
-                <li className='bg-primary text-white p-2 rounded-full hover:border hover:bg-white hover:border-primary cursor-pointer hover:text-black hover:space-x-2 transition-all ease-in lg:hidden'>
+                {/* <li className='bg-primary text-white p-2 rounded-full hover:border hover:bg-white hover:border-primary cursor-pointer hover:text-black hover:space-x-2 transition-all ease-in lg:hidden'>
                     <Search size={15}/>
-                </li>
+                </li> */}
                 <li className='flex  items-center gap-2'>
                   <Button asChild>
                     <Link href={'/addform'}>Add Past Question</Link>
